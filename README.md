4ab542e8a9526a9f17afdead7f797808f682aaee
haydarmiezanie <haydarsaja@gmail.com>
2022-11-09 10:12:24 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+
+# Red Me
+
+DailyTips scraping is scraper that aims data from DailyTips Article to xlsx
+
+## Deployment
+
+To deploy this you will need this
+
+[chromewebdriver](https://chromedriver.chromium.org/downloads)
+
+
+
+
+## Run Locally
+
+Clone the project
+
+
+Install dependencies
+
+```bash
+  pip install selenium
+  pip install pandas
+  pip install re
+```
+
+Start the server
+
+```bash
+  python -m dailytips_scraping.py
+```
+
+
+## Scraper Flow
+
+![App Screenshot](https://via.placeholder.com/468x300?text=App+Screenshot+Here)
+
+
+## Limitation
+
+This scraper only works in 1 [article](https://www.dailytips.id/2020/03/553-daftar-perusahaan-di-kawasan.html), the same method can be use in other article
+
+## Authors
+
+- [Haydar Miezanie](https://github.com/haydarmiezanie)
+
---
+++ dailytips_scraping.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+import re, openpyxl
+
+# Call chromdriver
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+# Define Function
+def main_function():
+    """
+    Function for start scraping.
+    """
+    print("Scraping start")
+    # Get driver
+    driver.get('https://www.dailytips.id/2020/03/553-daftar-perusahaan-di-kawasan.html')
+
+    # Find element by xpath
+    element_finder  = driver.find_element("xpath",'//*[@id="post-body-3992433005709954375"]/div[2]/ol')
+    element_to_text = element_finder.text
+    element_to_list = list(element_to_text.split("\n"))
+
+    # Find company_name using Regex
+    name_regex   = re.compile("PT")
+    name_newlist = list(filter(name_regex.match, element_to_list))
+
+    # Find company address using Regex
+    address_regex = re.compile("Alamat")
+    regex_newlist = list(filter(address_regex.match, element_to_list))
+
+    # Find company phone using Regex
+    phone_regex   = re.compile("Telp")
+    phone_newlist = list(filter(phone_regex.match, element_to_list))
+
+    # Make company_name and company_address in 1 excel
+    df = pd.DataFrame(list(zip(name_newlist,regex_newlist)),columns=['Nama PT','Adress'])
+    # Make phone_number in 1 excel
+    df2 = pd.DataFrame(list(zip(phone_newlist)),columns=['HP'])
+
+    df.to_excel("JABABEKA_1.xlsx") 
+    df2.to_excel("JABABEKA_2.xlsx")
+
+if __name__ == "__main__":
+    main_function()
+    print("Scraping is Done")
